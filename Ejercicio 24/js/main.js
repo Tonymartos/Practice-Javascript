315ba6bee6109c56da2454c05c738e38d53811fc
Tony Martos <Tonymartoscode@gmail.com>
2017-10-25 20:02:28 +0200

    Ejercicio 24 pendiente

--- Ejercicio 24/js/main.js
+++ Ejercicio 24/js/main.js
@@ -1,25 +1,16 @@
-function enlace(){
-  var opcion= document.getElementsByTagName('input')[i];
+window.onload = function(){
+  var formulario = document.getElementsByName('formulariocheck');
+  formulario.action = validarenlace()+".html";
 
-  for (var i=0; i<opcion.length; i++){
+}
+
+
+function validarenlace(){
+  var opcion= document.getElementsByTagName('input');
+  for ( i=0; i<opcion.length; i++){
     if (opcion[i].checked){
-      switch (opcion){
-        case 0:
-          opcion.onClick = "window.location='https://www.google.es/'";
-          break;
-        case 1:
-          opcion.onClick = "window.location='https://www.google.es/'";
-          break;
-        case 2:
-        opcion.onClick = "window.location='https://www.google.es/'";
-          break;
-        case 3:
-          opcion.onClick = "window.location='https://www.google.es/'";
-          break;
-        case 4:
-          opcion.onClick = "window.location='https://www.google.es/'";
-          break;
-      }
+      var pagina = opcion[i].value;
     }
   }
+  return pagina;
 }
