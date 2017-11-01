3ea73ed9cced9aaa1300690753642ef6d99c1ddc
Tony Martos <Tonymartoscode@gmail.com>
2017-11-01 19:25:20 +0100

    Ejercicios 23 y 24 terminados, 25 en proceso
    Ejercicios 23 y 24 terminados con un fallo, al checkear una casilla hay que recargar la pagina para que funcione

--- Ejercicio 24/js/main.js
+++ Ejercicio 24/js/main.js
@@ -1,16 +1,14 @@
 window.onload = function(){
-  var formulario = document.getElementsByName('formulariocheck');
-  formulario.action = validarenlace()+".html";
-
+  var formulario = document.getElementById('formulariocheck');
+  formulario.action = ""+validarenlace()+".html";
 }
 
-
 function validarenlace(){
-  var opcion= document.getElementsByTagName('input');
+  var opcion = document.getElementsByTagName('input');
   for ( i=0; i<opcion.length; i++){
     if (opcion[i].checked){
       var pagina = opcion[i].value;
+      return pagina;
     }
   }
-  return pagina;
 }
